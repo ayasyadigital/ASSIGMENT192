154bbf6f529563a0ea810a0452704482a472a790
Niket Goel <niket03@gmail.com>
2018-01-03 22:36:38 +0530

    Initial commit

---
+++ app/src/main/java/com/niketgoel/assigment192/data/remote/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.niketgoel.assigment192.data.remote;
+
+import com.niketgoel.assigment192.data.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
